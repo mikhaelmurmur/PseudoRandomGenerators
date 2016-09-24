1abd4187b7bbc24e569b048ff346e6e879337809
Mykhailo Stolovych <stolovich@yahoo.com>
2016-09-25 01:07:50 +0300

    Init structure of projects.

---
+++ PseudoRandomGenerators/PseudoRandomGenerators/PseudoRandomGenerators.cpp
@@ -0,0 +1,12 @@
+// PseudoRandomGenerators.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <NTL/zz.h>
+
+int main()
+{
+    NTL::ZZ p_s(10);
+    return 0;
+}
+
