7a9067a7ad8d3284eaf54f5ae072593ab31ffd5a
Mykhailo Stolovych <stolovich@yahoo.com>
2016-10-02 18:30:07 +0300

    Added factoryclass for generators

---
+++ PseudoRandomGenerators/PseudoRandomGenerators/GeneratorCreator.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "GeneratorCreator.h"
+#include "StandartGenerator.h"
+#include "BBSGenerator.h"
+#include "BBSGeneratorByte.h"
+#include "LehmerHighGenerator.h"
+#include "LehmerLowGenerator.h"
+#include "LibrarianGenerator.h"
+
+std::shared_ptr<IGeneratorBase> CGeneratorCreator::CreateGenerator(GeneratorType generatorType, lint seed) const
+{
+    switch (generatorType)
+    {
+    case Standart:
+        return std::make_shared<CStandartGenerator>(seed);
+    case BBS:
+        return std::make_shared<CBBSGenerator>(seed);
+    case BBSByte:
+        return std::make_shared<CBBSGeneratorByte>(seed);
+    case LehmarHigh:
+        return std::make_shared<CLehmerHighGenerator>(seed);
+    case LehmarLow:
+        return std::make_shared<CLehmerLowGenerator>(seed);
+    case Librarian:
+        return std::make_shared<CLibrarianGenerator>(seed);
+    default:
+        return std::make_shared<CStandartGenerator>(seed);
+    }
+}
---
+++ PseudoRandomGenerators/PseudoRandomGenerators/GeneratorCreator.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "IGeneratorBase.h"
+#include <memory>
+class CGeneratorCreator
+{
+public:
+    CGeneratorCreator() = default;
+    ~CGeneratorCreator() = default;
+    
+    enum GeneratorType
+    {
+        Standart,
+        LehmarHigh,
+        LehmarLow,
+        Librarian,
+        BBS,
+        BBSByte,
+        Count
+    };
+
+    std::shared_ptr<IGeneratorBase> CreateGenerator(GeneratorType generatorType,lint seed = lint(0))const;
+    
+};
+
