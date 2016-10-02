0993ca311805a46df247a8fdece5f41cd1f9215d
tur4in <turchyn.andrew.gmail.com>
2016-10-03 01:08:39 +0300

    fix

---
+++ PseudoRandomGenerators/PseudoRandomGenerators/LinearShiftRegister.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "LinearShiftRegister.h"
+
+
+
+
+int CLinearShiftRegister::GetNextBit()
+{
+    int nextBit = 0;
+    //for (int i = 0; i < m_registerCoefs.size ; ++i)
+    for(auto& coef: m_registerCoefs)
+    {
+        nextBit += m_registerState[coef];
+    }
+    int result = m_registerState.front();
+    m_registerState.pop_front();
+    m_registerState.push_back(nextBit);
+    return result % 2;
+    //прибрати в блоці
+}
